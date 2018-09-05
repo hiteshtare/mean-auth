0bfe7f7aae9d941977c4ad4c29a678fccd15f448
Hitesh Tare <hiteshtare@gmail.com>
2018-09-05 21:59:26 +0530

    mLab integration

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanauth',
+  //database: 'mongodb://localhost:27017/meanauth',
+  database: 'mongodb://trupti:admin123@ds145752.mlab.com:45752/mean-auth',
   secret: 'ysssecret' //For passport encryption
 };
